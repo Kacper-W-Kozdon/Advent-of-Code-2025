293804411c8047bfe004b387ccdd7ab6aecaf135
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-12-02 21:06:45 +0100

    day1

--- day1.py
+++ day1.py
@@ -11,8 +11,14 @@ def load_files(file_name):
     return fContent
 
 
-def password_finder(input, mod: int = 99):
-    dial = [num for num in range(0, mod - 1)]
+def password_finder(input: list[str], mod: int = 99, protocol: int = 0):
+    if not isinstance(input, list):
+        raise TypeError(f"Expected list, got {type(input)=}.")
+    if input == []:
+        raise ValueError("Input list is empty.")
+    if any([not isinstance(entry, str) for entry in input]):
+        raise TypeError(f"Entries in the input list contain incorrect types. Expected str, got: {set([type(entry) for entry in input])=}")
+    dial = [num for num in range(0, mod + 1)]
     solution = 0
 
     starting_position = 50
@@ -20,12 +26,36 @@ def password_finder(input, mod: int = 99):
 
     for instruction in input:
         instruction_numeric = int(instruction.replace("R", "+").replace("L", "-"))
-        next_number = dial[next_number + instruction_numeric]
-        if next_number == 0:
+        if protocol:
+            solution += abs((next_number + instruction_numeric) // (mod + 1))
+        next_number = dial[(next_number + instruction_numeric) % (mod + 1)]
+
+        if next_number == 0 and not protocol:
             solution += 1
 
     print(solution)
+    return solution
 
 
 def main():
-    input = 
+    input_test = load_files("test1.txt")
+
+    if (answer := password_finder(input_test)) != 3:
+        raise ValueError(f"Expected the answer to be 3. Got {answer=}")
+    
+    input = load_files("data1.txt")
+    answer = password_finder(input)
+
+    print(f"Solution 1 {answer=}.")
+
+    if (answer := password_finder(input_test, protocol=1)) != 6:
+        raise ValueError(f"Expected the answer to be 6. Got {answer=}")
+    
+    answer = password_finder(input, protocol=1)
+
+    print(f"Solution 2 {answer=}.")
+
+
+
+if __name__ == "__main__":
+    main()
