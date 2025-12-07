ad28b919dcc71d012b3bd51b6814dbea117168a1
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-12-07 17:46:25 +0100

    day6

--- day6.py
+++ day6.py
@@ -32,11 +32,55 @@ def load_files(file_name):
     return fContent
 
 
+class Math_Problems():
+
+    def __init__(self, input_data: list[str]):
+
+        self.puzzles = self.preprocess(input_data)
+
+    @timing
+    def solve_puzzles(self) -> int:
+
+        solution: int = 0
+
+        for puzzle in self.puzzles:
+            solution += int(eval(puzzle))
+
+        return solution
+
+    @classmethod
+    def preprocess(cls, input_data: list[str]) -> list[str]:
+
+        collapsed_puzzle_inputs: list[str] = []
+        symbols: list[str] = input_data[-1].split()
+
+        for index, puzzle_inputs in enumerate(input_data[:-1]):
+            
+            if index == 0:
+                collapsed_puzzle_inputs = [number for number in puzzle_inputs.split()]
+                continue
+
+            collapsed_puzzle_inputs = [f"{collapsed_puzzle_inputs[number_idx]} {symbols[number_idx]} {number}" for number_idx, number in enumerate(puzzle_inputs.split())]
+
+        return collapsed_puzzle_inputs
+
+
 def main():
     test_data = load_files("test6.txt")
     input_data = load_files("data6.txt")
 
-    print(test_data)
+    test_puzzles = Math_Problems(test_data)
+
+    test_solution = test_puzzles.solve_puzzles()
+
+    if test_solution != 4277556:
+        raise ValueError(f"Expected solution: 4277556. Got {test_solution=}.")
+    
+    puzzles = Math_Problems(input_data)
+
+    solution = puzzles.solve_puzzles()
+
+    print(f"The first problem's {solution=}.")
 
 
 if __name__ == "__main__":
